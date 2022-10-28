8de06129d46c5954a6e8aa43181bb018a8c715bf
sairak <sairakswk@gmail.com>
2022-10-28 16:25:19 +0700

    update search

--- src/views/admin/TableShowcase.js
+++ src/views/admin/TableShowcase.js
@@ -8,35 +8,14 @@ function TableShowcase() {
         <h1 className="text-left font-bold text-2xl my-2  text-gray-700">
           All Case
         </h1>
-        {/* <button
-          type="button"
-          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-        >
-          เพิ่มข้อมูล
-        </button> */}
       </div>
-
-      {/* <div className="mx-10 mt-2">
-        <button
-          type="button"
-          class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900"
-        >
-          Excel
-        </button>
-        <button
-          type="button"
-          class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900"
-        >
-          PDF
-        </button>
-      </div> */}
 
       {/* <div className="m-8 flex flex-col flex-grow pt-0  border-gray-400 border-dashed rounded"> */}
       <div className="m-8 flex flex-col  pt-0  border-gray-400 border-dashed rounded">
         <div className="overflow-hidden bg-white shadow sm:rounded-lg">
           <div className="px-4 py-5 sm:px-10">
             <form>
-              <div className="grid gap-6 mb-6 grid-cols-1 lg:grid-cols-5">
+              <div className="grid gap-6 mb-6 grid-cols-1 lg:grid-cols-4 md:grid-cols-2">
                 <div>
                   <label
                     htmlFor="date"
@@ -49,6 +28,25 @@ function TableShowcase() {
                     id="last_name"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="typecase"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                  >
+                    Priority
+                  </label>
+                  <select
+                    id="countries"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  >
+                    <option value="">เลือก</option>
+                    <option value="Critical">Critical</option>
+                    <option value="High">High</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Low">Low</option>
+                  </select>
                 </div>
 
                 <div>
@@ -81,6 +79,34 @@ function TableShowcase() {
 
                 <div>
                   <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    เลขทะเบียน (หน้า)
+                  </label>
+                  <input
+                    type="text"
+                    id="last_name"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    เลขทะเบียน (กลาง)
+                  </label>
+                  <input
+                    type="text"
+                    id="last_name"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  />
+                </div>
+
+                <div>
+                  <label
                     htmlFor="typecase"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
                   >
@@ -92,29 +118,9 @@ function TableShowcase() {
                   >
                     <option value="">เลือก</option>
                     <option value="US">Warning</option>
-                    <option value="CA">ส่งต่อให้ Noc</option>
-                    <option value="FR">ส่งต่อให้ Oper</option>
-                    <option value="FR">ส่งต่อให้ Dev</option>
+                    <option value="CA">ขอข้อมูลเพิ่มเติม</option>
+                    <option value="FR">รอ Dev</option>
                     <option value="DE">Success</option>
-                  </select>
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="typecase"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                  >
-                    Priority
-                  </label>
-                  <select
-                    id="countries"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  >
-                    <option value="">เลือก</option>
-                    <option value="Critical">Critical</option>
-                    <option value="High">High</option>
-                    <option value="Medium">Medium</option>
-                    <option value="Low">Low</option>
                   </select>
                 </div>
               </div>
